c94b6e37bad8914a66b2b40043de7348c838eb88
Tracy Lynn <tracy.lynn@boruapps.com>
2019-11-28 17:49:02 +0700

    43495

--- src/app/services/detail/detail.module.ts
+++ src/app/services/detail/detail.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule , IonicRouteStrategy} from '@ionic/angular';
 
 import { DetailPage } from './detail.page';
+
+import { RouteReuseStrategy } from '@angular/router';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+import { File } from '@ionic-native/file/ngx';
+import { Camera } from '@ionic-native/Camera/ngx';
 
 const routes: Routes = [
   {
@@ -21,6 +29,13 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [DetailPage]
+  declarations: [DetailPage],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        Camera,
+        File,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
 })
 export class DetailPageModule {}
--- src/app/services/detail/detail.page.ts
+++ src/app/services/detail/detail.page.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from  "@angular/router";
 import { NavController, ActionSheetController  } from '@ionic/angular';
 import { formatDate } from '@angular/common';
 import { Storage } from '@ionic/storage';
+import { Camera, CameraOptions } from '@ionic-native/Camera/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-detail',
@@ -41,7 +43,15 @@ export class DetailPage implements OnInit {
     endtime: ''
   }
     actionSheet:any;
-  constructor(public navCtrl: NavController, private  router:  Router, public storage: Storage, private activatedRoute: ActivatedRoute,public actionSheetController: ActionSheetController, @Inject(LOCALE_ID) private locale: string) { }
+  constructor(
+      public navCtrl: NavController,
+      private  router:  Router,
+      public storage: Storage,
+      private activatedRoute: ActivatedRoute,
+      public actionSheetController: ActionSheetController,
+      private camera: Camera,
+      private file: File,
+      @Inject(LOCALE_ID) private locale: string) { }
 
   loadDetails(serviceid){
     console.log('loading details for service id:', serviceid)
@@ -179,6 +189,23 @@ export class DetailPage implements OnInit {
       }
     });
   }
+    pickImage(sourceType) {
+        const options: CameraOptions = {
+            quality: 100,
+            sourceType: sourceType,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+        this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            // let base64Image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+        });
+    }
+
     openActionSheet() {
         this.actionSheet = this.actionSheetController.create({
             header: 'What would you like to do?',
@@ -187,11 +214,13 @@ export class DetailPage implements OnInit {
                 role: 'destructive',
                 handler: () => {
                     console.log('Take Photo clicked');
+                    this.pickImage(this.camera.PictureSourceType.CAMERA);
                 }
             }, {
                 text: 'Upload from Library',
                 handler: () => {
                     console.log('upload clicked');
+                    this.pickImage(this.camera.PictureSourceType.PHOTOLIBRARY);
                 }
             }, {
                 text: 'Cancel',
