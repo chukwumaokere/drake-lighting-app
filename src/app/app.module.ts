3be27f000d670484a6cb0c9c7b91aea98d641777
Tracy Lynn <tracy.lynn@boruapps.com>
2019-12-03 15:41:37 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,24 @@ import { IonicStorageModule } from '@ionic/storage';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { ActionSheet, ActionSheetOptions } from '@ionic-native/action-sheet/ngx';
 import { PhotoLibrary } from '@ionic-native/photo-library/ngx';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     Camera,
     ActionSheet,
-    PhotoLibrary
+    PhotoLibrary,
+      FileTransfer,
+      File
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/image-modal/image-modal.page.ts
+++ src/app/services/image-modal/image-modal.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController, NavParams } from '@ionic/angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-image-modal',
@@ -10,11 +14,24 @@ export class ImageModalPage implements OnInit {
 
 modalTitle:string;
 modelId:number;
+photo = {
+    title: '',
+    serviceid: '',
+    base64Image: ''
+    };
 
 constructor(
 private modalController: ModalController,
-private navParams: NavParams
-) { }
+private navParams: NavParams,
+public httpClient: HttpClient,
+private transfer: FileTransfer,
+private formBuilder: FormBuilder
+) {
+    /*this.photo = this.formBuilder.group({
+        title: ['', Validators.required],
+        serviceid: [''],
+    });*/
+}
 
   ngOnInit() {
   console.table(this.navParams);
@@ -27,7 +44,42 @@ private navParams: NavParams
     await this.modalController.dismiss(onClosedData);
   }
 
-  async  uploadImage(){
+  async  uploadImage(form){
+      console.log('form submitted',form.value);
+      /*const fileTransfer: FileTransferObject = this.transfer.create();
+      let options: FileUploadOptions = {
+          fileKey: "photo",
+          fileName: "test3.jpg",
+          chunkedMode: false,
+          mimeType: "image/jpeg",
+          headers: {}
+      }
+      fileTransfer.upload(this.base64img, 'http://localhost/drakelighting/phoneapi/postPhotos.php', options).then(data => {
+          alert(JSON.stringify(data));
+      }, error => {
+          alert("error");
+          alert("error" + JSON.stringify(error));
+      });*/
+      const requestOptions = {
+          headers: new HttpHeaders({
+              'Content-Type':  'application/json',
+              'Accept': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+          })
+      };
+
+      this.httpClient.post("https://devl06.borugroup.com/drakelighting/phoneapi/postPhotos.php", form.value, requestOptions)
+          .subscribe(data => {
+              console.log(data['_body']);
+          }, error => {
+              console.log(error);
+          });
+
+
     this.closeModal();
+  }
+
+  async  fillTitle(title){
+      this.photo.title = title;
   }
 }
