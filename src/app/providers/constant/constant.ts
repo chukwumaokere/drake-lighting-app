53c5405381ba241798457e07395697514aa4b722
Tracy Lynn <tracy.lynn@boruapps.com>
2019-12-24 14:56:01 +0700

    fix save piclist in detail page, change api url to production

--- src/app/providers/constant/constant.ts
+++ src/app/providers/constant/constant.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConstants  {
-   apiurl : string = 'http://devl06.borugroup.com/drakelighting/phoneapi/';
+   apiurl : string = 'https://www.drakelighting.com/sales/phoneapi/';
     getApiUrl(){
         return this.apiurl;
     }
