b5017ddd8f068c01d0f51b77b59d05a9ed8bdebc
Tracy Lynn <tracy.lynn@boruapps.com>
2019-12-25 09:47:32 +0700

    update production URL

--- src/app/providers/constant/constant.ts
+++ src/app/providers/constant/constant.ts
@@ -3,7 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppConstants  {
    apiurl : string = 'https://www.drakelighting.com/sales/phoneapi/';
+   vturl : string = 'https://www.drakelighting.com/sales/';
     getApiUrl(){
         return this.apiurl;
+    }
+    getVtUrl(){
+        return this.vturl;
     }
 }
