d06b4b62134450e9d3c54894b8567b459f98b7bd
tuyen.vv <tuyen.vv@boruapps.com>
2020-01-06 10:58:52 +0700

    update loading

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,124 +1,126 @@
-import { Component, OnInit, LOCALE_ID, Inject, } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
-import { NavController, ModalController } from '@ionic/angular';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Storage } from '@ionic/storage';
-import { AppConstants } from '../providers/constant/constant';
-import { LoadingController } from '@ionic/angular';
+import {Component, OnInit, LOCALE_ID, Inject,} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {NavController, ModalController} from '@ionic/angular';
+import {HttpHeaders, HttpClient} from '@angular/common/http';
+import {Storage} from '@ionic/storage';
+import {AppConstants} from '../providers/constant/constant';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html',
+    styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  underreview: any;
-  apiurl: any;
-  user_id: any;
-  userinfo: any;
+    underreview: any;
+    apiurl: any;
+    user_id: any;
+    userinfo: any;
 
-  constructor(
-    private httpClient: HttpClient,
-    public navCtrl: NavController,
-    private router: Router,
-    public storage: Storage,
-    private activatedRoute: ActivatedRoute,
-    public appConst: AppConstants,
-    public modalCtrl: ModalController,
-    public loadingController: LoadingController
-  ) {
-    this.apiurl = this.appConst.getApiUrl();
-  }
-
-  loading: any;
-
-  async showLoading() {
-    this.loading = await this.loadingController.create({
-      message: 'Loading ...'
-    });
-    this.loading.present();
-  }
-
-  async hideLoading() {
-    this.loading.dismiss();
-  }
-
-  async isLogged() {
-    var log_status = this.storage.get('userdata').then((userdata) => {
-      if (userdata && userdata.length !== 0) {
-        return userdata;
-      } else {
-        return false;
-      }
-    })
-    return log_status;
-  }
-  logout() {
-    console.log('logout clicked');
-    this.storage.set("userdata", null);
-    this.router.navigateByUrl('/login');
-  }
-
-  getWorkOrders(user_id, type) {
-    var logged_user = {
-      user_id: user_id,
-      type: type
+    constructor(
+        private httpClient: HttpClient,
+        public navCtrl: NavController,
+        private router: Router,
+        public storage: Storage,
+        private activatedRoute: ActivatedRoute,
+        public appConst: AppConstants,
+        public modalCtrl: ModalController,
+        public loadingController: LoadingController
+    ) {
+        this.apiurl = this.appConst.getApiUrl();
     }
-    console.log('fetching records for', logged_user);
-    var headers = new HttpHeaders();
-    headers.append("Accept", 'application/json');
-    headers.append('Content-Type', 'application/x-www-form-urlencoded');
-    headers.append('Access-Control-Allow-Origin', '*');
-    this.showLoading();
-    this.httpClient.post(this.apiurl + "getWorkOrders.php", logged_user, { headers: headers, observe: 'response' })
-      .subscribe(data => {
-        this.hideLoading();
-        console.log(data['body']);
-        var success = data['body']['success'];
-        console.log('login response was', success);
 
-        if (success == true) {
-          var workorders = data['body']['data'];
-          console.log('workorders', workorders);
-          if (type == 'underreview') {
-            if(workorders){
-              workorders.forEach(workorder => {
-                workorder['longdate'] = workorder['date_start'] + ' ' + workorder['time_start'];
-              });
-            }
-            //this.underreview= workorders;
-            this.underreview = data['body']['count'];
-            //console.log('weekly services,', this.weeklyServices);
-          }
-        } else {
-          console.log('failed to fetch records');
-        }
+    loading: any;
 
-      }, error => {
-        this.hideLoading();
-        //console.log(error);
-        //console.log(error.message);
-        //console.error(error.message);
-        console.log('failed to fetch records');
-      });
-  }
+    async showLoading() {
+        this.loading = await this.loadingController.create({
+            message: 'Loading ...'
+        });
+        this.loading.present();
+    }
 
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((userData) => {
-      if (userData.length !== 0) {
-        this.userinfo = userData;
-        if (userData.length == undefined) {
-          this.isLogged().then(result => {
-            if (!(result == false)) {
-              this.userinfo = result;
-              this.getWorkOrders(this.userinfo.id, 'underreview');
-              this.user_id = this.userinfo.id;
+    async hideLoading() {
+        if (this.loading != undefined)
+            this.loading.dismiss();
+    }
+
+    async isLogged() {
+        var log_status = this.storage.get('userdata').then((userdata) => {
+            if (userdata && userdata.length !== 0) {
+                return userdata;
             } else {
-              this.logout();
+                return false;
             }
-          })
+        })
+        return log_status;
+    }
+
+    logout() {
+        console.log('logout clicked');
+        this.storage.set("userdata", null);
+        this.router.navigateByUrl('/login');
+    }
+
+    getWorkOrders(user_id, type) {
+        var logged_user = {
+            user_id: user_id,
+            type: type
         }
-      }
-    });
-  }
+        console.log('fetching records for', logged_user);
+        var headers = new HttpHeaders();
+        headers.append("Accept", 'application/json');
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        headers.append('Access-Control-Allow-Origin', '*');
+        this.showLoading();
+        this.httpClient.post(this.apiurl + "getWorkOrders.php", logged_user, {headers: headers, observe: 'response'})
+            .subscribe(data => {
+                this.hideLoading();
+                console.log(data['body']);
+                var success = data['body']['success'];
+                console.log('login response was', success);
+
+                if (success == true) {
+                    var workorders = data['body']['data'];
+                    console.log('workorders', workorders);
+                    if (type == 'underreview') {
+                        if (workorders) {
+                            workorders.forEach(workorder => {
+                                workorder['longdate'] = workorder['date_start'] + ' ' + workorder['time_start'];
+                            });
+                        }
+                        //this.underreview= workorders;
+                        this.underreview = data['body']['count'];
+                        //console.log('weekly services,', this.weeklyServices);
+                    }
+                } else {
+                    console.log('failed to fetch records');
+                }
+
+            }, error => {
+                this.hideLoading();
+                //console.log(error);
+                //console.log(error.message);
+                //console.error(error.message);
+                console.log('failed to fetch records');
+            });
+    }
+
+    ngOnInit() {
+        this.activatedRoute.params.subscribe((userData) => {
+            if (userData.length !== 0) {
+                this.userinfo = userData;
+                if (userData.length == undefined) {
+                    this.isLogged().then(result => {
+                        if (!(result == false)) {
+                            this.userinfo = result;
+                            this.getWorkOrders(this.userinfo.id, 'underreview');
+                            this.user_id = this.userinfo.id;
+                        } else {
+                            this.logout();
+                        }
+                    })
+                }
+            }
+        });
+    }
 }
