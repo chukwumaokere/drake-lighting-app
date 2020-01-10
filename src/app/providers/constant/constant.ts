b089edb1e6863bb86844e9289a857186b3544c72
chukwumaokere <chukwumaokere@yahoo.com>
2020-01-10 17:21:15 -0600

    update current_updates prod 1.7.3

--- src/app/providers/constant/constant.ts
+++ src/app/providers/constant/constant.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConstants  {
-    apiurl : string = 'https://devl06.borugroup.com/drakelighting/phoneapi/';
-    vturl : string = 'https://devl06.borugroup.com/drakelighting/';
+    apiurl : string = 'https://www.drakelighting.com/sales/phoneapi/';
+    vturl : string = 'https://www.drakelighting.com/sales/';
     getApiUrl() {
         return this.apiurl;
     }
