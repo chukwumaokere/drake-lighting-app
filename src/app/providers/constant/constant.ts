fd2420d1c8ae3ae5b5bb1025e9c8839c7737d0bd
chukwumaokere <chukwumaokere@yahoo.com>
2020-01-20 09:01:12 -0600

    pulling 1.7.4 from production

--- src/app/providers/constant/constant.ts
+++ src/app/providers/constant/constant.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConstants  {
-    apiurl : string = 'https://www.drakelighting.com/sales/phoneapi/';
-    vturl : string = 'https://www.drakelighting.com/sales/';
+    apiurl : string = 'https://devl06.borugroup.com/drakelighting/phoneapi/';
+    vturl : string = 'https://devl06.borugroup.com/drakelighting/';
     getApiUrl() {
         return this.apiurl;
     }
